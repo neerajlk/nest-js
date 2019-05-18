6ac8f0b2bd906d2bac31167664a63aefb3461df6
neerajwebdev <you@example.com>
2019-05-19 03:39:45 +0530

    creating directories

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//controllers
+import { ItemsController } from './items/items.controller';
+import { ShoppingCartController } from './shopping-cart/shopping-cart.controller';
+
+//services
+import { ItemsService } from './items/items.service';
+
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,ItemsController,ShoppingCartController],
+  providers: [AppService,ItemsService],
 })
 export class AppModule {}
